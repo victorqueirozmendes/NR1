f948d4627516a7741575c6b15637b3a796042d81
Victor007700 <>
2026-01-31 13:33:17 -0300

    up

---
+++ script.js
@@ -0,0 +1,6 @@
+// Script simples para futuras interações
+// Exemplo: animações, botões, scroll suave
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Site PMN Soluções Corporativas carregado com sucesso");
+});
